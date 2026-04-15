7888be904fac302fe5ccd8e5ab2ac0bbe395972f
JoelxDev <joeladriano.jt@gmail.com>
2026-04-14 21:01:28 -0500

    Se implemento el registro de movimientos para categorias

--- backend/src/modules/categorias/categorias.controller.js
+++ backend/src/modules/categorias/categorias.controller.js
@@ -1,9 +1,12 @@
 import * as CategoriasModel from './categorias.model.js';
+import { insertarMovimiento, TIPOS_MOVIMIENTO } from '../movimientos/movimientos.model.js';
 
 export async function crearCategoria(req, res) {
     try {
         const nuevoId = await CategoriasModel.crearCategoria(req.body);
         res.status(201).json({ id: nuevoId, mensaje: "Categoría creada exitosamente" });
+        // console.log(req.body)
+        await insertarMovimiento(TIPOS_MOVIMIENTO.CREACION, 'Categoria : ' + req.body.nombre_categoria, req.usuario.id); // Registramos el movimiento de creación de categoría
     } catch (error) {
         console.error("❌ Error al crear categoría:", error);
         res.status(500).json({ mensaje: "Error al crear la categoría", error: error.message });
@@ -37,10 +40,28 @@ export async function obtenerCategoriaPorId(req, res) {
 export async function actualizarCategoria(req, res) {
     try {
         const { idCategoria } = req.params;
+        const categoriaAnterior = await CategoriasModel.obtenerCategoriaPorId(idCategoria); 
+        if (!categoriaAnterior) {
+            return res.status(404).json({ mensaje: "Categoría no encontrada" });
+        }
         const filasAfectadas = await CategoriasModel.actualizarCategoria(idCategoria, req.body);
         if (filasAfectadas === 0) {
             return res.status(404).json({ mensaje: "Categoría no encontrada" });
         }
+        let cambios = [];
+        const remasterizar = {
+            nombre_categoria: "Nombre",
+            descripcion_categoria: "Descripción",
+        }
+        for (const nombPropiedad in req.body){
+            if (req.body[nombPropiedad] !== categoriaAnterior[nombPropiedad]){
+                 cambios.push(`${remasterizar[nombPropiedad]}: ${categoriaAnterior[nombPropiedad]} -> ${req.body[nombPropiedad]}`);
+            }
+        }
+
+        const detalles = cambios.length > 0 ? cambios.join(', ') : 'Sin cambios detectados';
+        await insertarMovimiento(TIPOS_MOVIMIENTO.EDICION, detalles, req.usuario.id); // Registramos el movimiento de edición de categoría con los cambios realizados
+
         res.json({ mensaje: "Categoría actualizada correctamente" });
     } catch (error) {
         console.error("❌ Error al actualizar categoría:", error);
@@ -51,10 +72,12 @@ export async function actualizarCategoria(req, res) {
 export async function eliminarCategoria(req, res) {
     try {
         const { idCategoria } = req.params;
+        const categoriaAnterior = await CategoriasModel.obtenerCategoriaPorId(idCategoria);
         const filasAfectadas = await CategoriasModel.eliminarCategoria(idCategoria);
         if (filasAfectadas === 0) {
             return res.status(404).json({ mensaje: "Categoría no encontrada" });
         }
+        await insertarMovimiento(TIPOS_MOVIMIENTO.ELIMINACION, 'Categoría : ' + categoriaAnterior.nombre_categoria, req.usuario.id); 
         res.json({ mensaje: "Categoría eliminada exitosamente" });
     } catch (error) {
         console.error("❌ Error al eliminar categoría:", error);
